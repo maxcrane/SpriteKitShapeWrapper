cdaea021dbf9daf2fc678645387bb913cff3663d
maxcrane <Maxlccrane@gmail.com>
2015-12-15 18:05:36 -0800

    Add Bin class for dropping shapes into a trash.

---
+++ ShapeWrapper/ShapeWrapper/Bin.swift
@@ -0,0 +1,20 @@
+//
+//  Bin.swift
+//  ShapeWrapper
+//
+//  Created by Max Crane on 12/15/15.
+//  Copyright © 2015 Crane Apps. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Bin: SKShapeNode{
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(){
+        super.init()
+    }
+}
