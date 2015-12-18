a87f00fad5f78427921082c364bcf3f009beee71
maxcrane <Maxlccrane@gmail.com>
2015-12-18 14:45:36 -0800

    Correct bug in CollisionBitMask in ShapeUtils.

--- ShapeWrapper/ShapeWrapper/ShapeUtil.swift
+++ ShapeWrapper/ShapeWrapper/ShapeUtil.swift
@@ -25,39 +25,9 @@ class ShapeUtil {
         
     }
     
-    static func rotateFromVectors(var v1: CGVector, var v2: CGVector, aShape: SKShapeNode){
-        if(v1.dx < 0 && v2.dx > 0){
-            v1.dx = -v1.dx
-        }
-        else if(v1.dx > 0 && v2.dx < 0){
-            v2.dx = -v2.dx
-        }
-        
-        let firstAngle = atan(v1.dy/v1.dx)
-        let secondAngle = atan(v2.dy/v2.dx)
-        let angleDifference = secondAngle - firstAngle
-
-        rotateBy(angleDifference, aShape: aShape)
-    }
-    
-    static func scaleFromVectors( v1: CGVector,  v2: CGVector, aShape: SKShapeNode){
-        let v1Length = sqrt(pow(v1.dx, 2.0) + pow(v1.dy, 2.0))
-        let v2Length = sqrt(pow(v2.dx, 2.0) + pow(v2.dy, 2.0))
-        let lengthDiff = v2Length - v1Length
-        var scaleDiff = 0.0
-        
-        if(lengthDiff > 0){
-            scaleDiff = 0.1
-        }
-        else{
-            scaleDiff = -0.1
-        }
-        
-        aShape.setScale(CGFloat(1.0 + scaleDiff))
-    }
     
     static func fadeOut(aShape: SKShapeNode){
-        let fadeOutAction = SKAction.fadeAlphaTo(0.5, duration: 0.3)
+        let fadeOutAction = SKAction.fadeAlphaTo(0.3, duration: 0.3)
         aShape.runAction(fadeOutAction)
     }
     
@@ -67,7 +37,7 @@ class ShapeUtil {
     }
     
     static func collisionBitMasks()->UInt32{
-        return CollisionCategories.Border & CollisionCategories.Circle & CollisionCategories.Ellipse
-            & CollisionCategories.Rectangle & CollisionCategories.Triangle
+        return CollisionCategories.Border | CollisionCategories.Circle | CollisionCategories.Ellipse
+            | CollisionCategories.Rectangle | CollisionCategories.Triangle
     }
 }
