21030c0625007eea72b89e9297272d5cfb737a41
maxcrane <Maxlccrane@gmail.com>
2015-12-19 13:54:42 -0800

    Add star class

---
+++ ShapeWrapper/ShapeWrapper/Star.swift
@@ -0,0 +1,22 @@
+//
+//  Star.swift
+//  ShapeWrapper
+//
+//  Created by Max Crane on 12/19/15.
+//  Copyright © 2015 Crane Apps. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Star: SKShapeNode {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(){
+        super.init()
+        
+    }
+    
+}
